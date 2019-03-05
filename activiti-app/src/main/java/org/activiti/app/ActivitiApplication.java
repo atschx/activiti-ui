ff4f9bda02d45e922801faaa90bfce989c00ad44
Albert <albert.sun@guanaitong.com>
2019-03-05 11:22:18 +0800

    add springBoot

---
+++ activiti-app/src/main/java/org/activiti/app/ActivitiApplication.java
@@ -0,0 +1,68 @@
+package org.activiti.app;
+
+import org.activiti.app.conf.ApplicationConfiguration;
+import org.activiti.app.servlet.ApiDispatcherServletConfiguration;
+import org.activiti.app.servlet.AppDispatcherServletConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Created by Albert on 2019/3/5.
+ */
+@Import(ApplicationConfiguration.class)
+@SpringBootApplication
+public class ActivitiApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder().sources(ActivitiApplication.class).run(args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ActivitiApplication.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean api() {
+
+        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+
+        DispatcherServlet api = new DispatcherServlet();
+        api.setContextClass(AnnotationConfigWebApplicationContext.class);
+        api.setContextConfigLocation(ApiDispatcherServletConfiguration.class.getName());
+
+        servletRegistrationBean.setServlet(api);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.setAsyncSupported(true);
+        servletRegistrationBean.addUrlMappings("/api/**");
+        servletRegistrationBean.setName("api");
+
+        return servletRegistrationBean;
+    }
+
+
+    @Bean
+    public ServletRegistrationBean app() {
+        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+
+        DispatcherServlet app = new DispatcherServlet();
+        app.setContextClass(AnnotationConfigWebApplicationContext.class);
+        app.setContextConfigLocation(AppDispatcherServletConfiguration.class.getName());
+
+        servletRegistrationBean.setServlet(app);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.setAsyncSupported(true);
+        servletRegistrationBean.addUrlMappings("/app/**");
+        servletRegistrationBean.setName("app");
+        return servletRegistrationBean;
+    }
+
+
+}
